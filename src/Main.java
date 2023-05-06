cd9b251ede75424df87ff872d7b4e4b7818eb989
JDRomano <j.romano@campus.fct.unl.pt>
2023-05-06 22:02:07 +0100

    Codigo corre, da sempre stuck idk why ainda. btw nao precisamos do union find panada

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static final String STUCK = "Stuck";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
@@ -10,16 +12,30 @@ public class Main {
         int cols = Integer.parseInt(in[1]);
         int numTests = Integer.parseInt(in[2]);
         char[][] map = new char[rows][cols];
+        int xH = Integer.MAX_VALUE, yH = Integer.MAX_VALUE;
         for (int i = 0; i < rows; i++) {
             String row = br.readLine();
-            for (int j = 0; j < cols; j++)
+            for (int j = 0; j < cols; j++) {
                 map[i][j] = row.charAt(j);
+            }
         }
+        Solution solution = new Solution(map);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+
         for (int k = 0; k < numTests; k++) {
             String[] input = br.readLine().split(" ");
-            int xInit = Integer.parseInt(input[0]);
-            int yInit = Integer.parseInt(input[1]);
-
+            int xInit = Integer.parseInt(input[0]) - 1;
+            int yInit = Integer.parseInt(input[1]) - 1;
+            solution.hasPath(xInit, yInit);
+            if (solution.hasPath(xInit, yInit))
+                System.out.println(solution.getNumPlays());
+            else
+                System.out.println(STUCK);
         }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,15 +3,13 @@ import java.util.Queue;
 
 public class Solution {
     private char[][] map;
-    private int[] hole;
     private boolean[][] visited;
     private Queue<int[]> q;
     private int numPlays;
     int[][] dirs;
 
 
-    public Solution(char[][] map, int xH, int yH) {
-        hole = new int[]{xH, yH};
+    public Solution(char[][] map) {
         q = new LinkedList<>();
         this.map = map;
         visited = new boolean[map.length][map[0].length];
@@ -20,14 +18,13 @@ public class Solution {
     }
 
     public boolean hasPath(int xInit, int yInit) {
+        numPlays = 0;
         int[] start = {xInit, yInit};
         visited[start[0]][start[1]] = true;
         q.offer(start);
 
         while (!q.isEmpty()) {
             int[] curr = q.poll();
-            //verificar se ja chegamos ao H
-            if (curr[0] == hole[0] && curr[1] == hole[1]) return true;
 
             for (int[] dir : dirs) {
                 int x = dir[0] + curr[0];
@@ -39,6 +36,11 @@ public class Solution {
                 numPlays++;
                 x -= dir[0];
                 y -= dir[1];
+                //verificar se ja chegamos ao H
+                if (map[curr[0]][curr[1]] == 'H'){
+                    return true;
+                }
+
                 if (!visited[x][y]) {
                     q.offer(new int[]{x, y});
                     visited[x][y] = true;
