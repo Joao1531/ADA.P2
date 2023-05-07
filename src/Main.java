df4475576991be7665decd672bfad5e7da6d9b28
JDRomano <j.romano@campus.fct.unl.pt>
2023-05-07 19:15:06 +0100

    asdasd

--- src/Main.java
+++ src/Main.java
@@ -57,7 +57,6 @@ public class Main {
                 System.out.println("TRUE");
             else
                 System.out.println(STUCK);
-
         }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -11,29 +11,33 @@ public class Solution {
         queue.add(start);
         visited[start[0]][start[1]] = true;
         numPlays = 0;
+        int [] countPlays = new int[4];
         while (!queue.isEmpty()) {
             int[] s = queue.remove();
-            //System.out.println("CURR COORDS: " + s[0] + " - " + s[1]);
-
+            System.out.println("CURR COORDS: " + s[0] + " - " + s[1]);
+            numPlays++;
             // The furthest point in four directions
             for (int[] dir : dirs) {
+
                 int x = s[0] + dir[0];
                 int y = s[1] + dir[1];
 
                 // move Furthest straight-line distance
                 while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && (maze[x][y] == 0 || maze[x][y] == 2)) {
-                    x += dir[0];
-                    y += dir[1];
-                    if (maze[x - dir[0]][y - dir[1]] == 2) {
+                    if (maze[x][y] == 2) {
                         System.out.println("Number of wall hits on correct path: " + numPlays);
                         return true;
                     }
+                    x += dir[0];
+                    y += dir[1];
                 }
-                if (maze[x - dir[0]][y-dir[1]] == 1) numPlays++;
+                x -= dir[0];
+                y -= dir[1];
+
                 // Furthest (x,y)
-                if (!visited[x - dir[0]][y - dir[1]]) {
-                    queue.add(new int[]{x - dir[0], y - dir[1]});
-                    visited[x - dir[0]][y - dir[1]] = true;
+                if (!visited[x][y]) {
+                    queue.add(new int[]{x, y});
+                    visited[x][y] = true;
                 }
             }
         }
