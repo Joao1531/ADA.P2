ad41ba24050f8a51f32246f1b52d25b2736218ee
Joao Lopes <jpdu.lopes@campus.fct.unl.pt>
2023-05-08 10:54:45 +0100

    DONEZO

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     private static final String STUCK = "Stuck";
@@ -17,13 +15,11 @@ public class Main {
         /* CREATE STRUCTS */
         int[][] map = new int[rows][cols];
         int xH = Integer.MAX_VALUE, yH = Integer.MAX_VALUE;
-        List<String> results = new ArrayList<>();
 
         /* RECEBER LINHAS DO MAPA DE INPUT E CONVERTER PARA MAPA DE INTS PARA FACILITAR */
         for (int i = 0; i < rows; i++) {
             //RECEBE LINHA
             String row = br.readLine();
-
 
             for (int j = 0; j < cols; j++) {
                 //POR CADA INDEX CONVERTE '.' -> 0 || 'O' -> 1 || 'H'-> 2
@@ -40,26 +36,26 @@ public class Main {
         }
         Solution solution = new Solution();
         //Print do mapa
-
+        /*for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }*/
 
         for (int k = 0; k < numTests; k++) {
             String[] input = br.readLine().split(" ");
-
+            // System.out.println(input[0] + "+" + input[1]);
 
             int xInit = Integer.parseInt(input[0]) - 1;
             int yInit = Integer.parseInt(input[1]) - 1;
-
+            //System.out.println("HOLE: " + xH + " - " + yH);
+            //System.out.println("INICIO: " + xInit + " - " + yInit);
 
             if (solution.hasPath(map, new int[]{xInit, yInit}, new int[]{xH, yH}))
-                results.add(String.valueOf(solution.getMinPlays()));
+                System.out.println(solution.getNumPlays());
             else
-                results.add(STUCK);
-
-            
+                System.out.println(STUCK);
         }
-        for (String result : results) {
-            System.out.println(result);
-        }
-
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,63 +2,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    private int minPlays;
-    private static int[] UP = {-1, 0};
-    private static int[] DOWN = {1,0};
-    private static int[] LEFT = {0,-1};
-    private static int[] RIGHT = {0,1};
+    private int numPlays;
+    private boolean flag;
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         boolean[][] visited = new boolean[maze.length][maze[0].length];
-        int[][] dirs = new int[][]{UP, DOWN, LEFT, RIGHT};
-        Queue<MazeSphere> queue = new LinkedList();
-        queue.offer(new MazeSphere(start[0], start[1], 0));
+        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        Queue<int[]> queue = new LinkedList();
+        queue.add(start);
         visited[start[0]][start[1]] = true;
-        minPlays = Integer.MAX_VALUE;
+        numPlays = 0;
+        flag = false;
 
         while (!queue.isEmpty()) {
+            int level_size = queue.size();
+            while (level_size-- != 0) {
+                int[] s = queue.poll();
+                // The furthest point in four directions
+                for (int[] dir : dirs) {
+                    int x = s[0] + dir[0];
+                    int y = s[1] + dir[1];
+                    //System.out.println("Estas são as coordenadas do nó da queue + as coordenadas da direcao " + x + "<->" + y);
 
-            MazeSphere sphere = queue.poll();
-            //System.out.println("CURR COORDS: " + sphere.x + " - " + sphere.y);
-
-
-            // The furthest point in four directions
-            for (int[] dir : dirs) {
-                //System.out.println(sphere.x + " - " + sphere.y);
-                int x = sphere.x + dir[0];
-                int y = sphere.y + dir[1];
-                //System.out.println(x + " - " + y);
-
-                // move Furthest straight-line distance
-                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && (maze[x][y] == 0 || maze[x][y] == 2)) {
-                    if (maze[x][y] == 2) {
-                        sphere.numPlays++;
-                        if (sphere.numPlays < minPlays)
-                            minPlays = sphere.numPlays;
-                        // System.out.println("Number of wall hits on correct path: " + minPlays);
-                        return true;
+                    // move Furthest straight-line distance
+                    while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && (maze[x][y] != 1)) {
+                        if (maze[x][y] == 2) {
+                            flag = true;
+                            break;
+                        }
+                        x += dir[0];
+                        y += dir[1];
                     }
-                    x += dir[0];
-                    y += dir[1];
+                    x -= dir[0];
+                    y -= dir[1];
+                    // Furthest (x,y)
+                    if (!visited[x][y]) {
+                        //System.out.println(x + " - " + y);
+                        queue.add(new int[]{x, y});
+                        visited[x][y] = true;
+                    }
                 }
-                x -= dir[0];
-                y -= dir[1];
-                //tou cofiante
-                // Furthest (x,y)
-                if (!visited[x][y]) {
-                    //System.out.println(x + " - " + y);
-                    sphere.numPlays++;
-                    // System.out.println("NUM PLAYS: " + sphere.numPlays);
-                    queue.add(new MazeSphere(x, y, sphere.numPlays));
-                    visited[x][y] = true;
-                }
-                System.out.println("NUM PLAYS: " + sphere.numPlays);
             }
-        }
-        return false;
+            numPlays++;
+            if (flag) break;
+            //System.out.println("curr level: " + numPlays);
+        }//
+        return flag;
     }
 
-    public int getMinPlays() {
-        return minPlays;
+    public int getNumPlays() {
+        return numPlays;
     }
 }
