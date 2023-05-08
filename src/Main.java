fb3e082a16b61ad2ad758b0c227341bc33563fbd
Joao Lopes <jpdu.lopes@campus.fct.unl.pt>
2023-05-08 16:57:17 +0100

    aiaiaiai muchachoooo

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
-        /* DISASSEMBLE INPUT */
         int rows = Integer.parseInt(in[0]);
         int cols = Integer.parseInt(in[1]);
         int numTests = Integer.parseInt(in[2]);
-        /* CREATE STRUCTS */
         int[][] map = new int[rows][cols];
         int xH = Integer.MAX_VALUE, yH = Integer.MAX_VALUE;
 
-        /* RECEBER LINHAS DO MAPA DE INPUT E CONVERTER PARA MAPA DE INTS PARA FACILITAR */
         for (int i = 0; i < rows; i++) {
-            //RECEBE LINHA
             String row = br.readLine();
 
             for (int j = 0; j < cols; j++) {
-                //POR CADA INDEX CONVERTE '.' -> 0 || 'O' -> 1 || 'H'-> 2
                 if (row.charAt(j) == '.') map[i][j] = 0;
                 else if (row.charAt(j) == 'O') map[i][j] = 1;
                 else if (row.charAt(j) == 'H') map[i][j] = 2;
@@ -35,23 +30,14 @@ public class Main {
             }
         }
         Solution solution = new Solution();
-        //Print do mapa
-        /*for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }*/
+
 
         for (int k = 0; k < numTests; k++) {
             String[] input = br.readLine().split(" ");
-            // System.out.println(input[0] + "+" + input[1]);
+
 
             int xInit = Integer.parseInt(input[0]) - 1;
             int yInit = Integer.parseInt(input[1]) - 1;
-            //System.out.println("HOLE: " + xH + " - " + yH);
-            //System.out.println("INICIO: " + xInit + " - " + yInit);
-
             if (solution.hasPath(map, new int[]{xInit, yInit}, new int[]{xH, yH}))
                 System.out.println(solution.getNumPlays());
             else
--- src/Solution.java
+++ src/Solution.java
@@ -2,42 +2,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    private int numPlays;
-    private boolean flag;
+    private int minPlays;
+    private static final int[] UP = {-1, 0};
+    private static final int[] DOWN = {1, 0};
+    private static final int[] LEFT = {0, -1};
+    private static final int[] RIGHT = {0, 1};
+
+    public Solution() {
+    }
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         boolean[][] visited = new boolean[maze.length][maze[0].length];
-        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-        Queue<int[]> queue = new LinkedList();
-        queue.add(start);
+        int[][] dirs = new int[][]{UP, DOWN, LEFT, RIGHT};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(start);
         visited[start[0]][start[1]] = true;
-        numPlays = 0;
-        flag = false;
+        int numPlays = 0;
+        boolean flag = false;
+
 
         while (!queue.isEmpty()) {
             int level_size = queue.size();
             while (level_size-- != 0) {
                 int[] s = queue.poll();
-                // The furthest point in four directions
                 for (int[] dir : dirs) {
                     int x = s[0] + dir[0];
                     int y = s[1] + dir[1];
-                    //System.out.println("Estas são as coordenadas do nó da queue + as coordenadas da direcao " + x + "<->" + y);
-
-                    // move Furthest straight-line distance
                     while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && (maze[x][y] != 1)) {
-                        if (maze[x][y] == 2) {
+                        if (x == destination[0] && y == destination[1]) {
                             flag = true;
                             break;
                         }
                         x += dir[0];
                         y += dir[1];
                     }
+                    if(x < 0 || y < 0 || x >= maze.length || y >= maze[0].length)continue;
                     x -= dir[0];
                     y -= dir[1];
-                    // Furthest (x,y)
                     if (!visited[x][y]) {
-                        //System.out.println(x + " - " + y);
                         queue.add(new int[]{x, y});
                         visited[x][y] = true;
                     }
@@ -45,12 +47,12 @@ public class Solution {
             }
             numPlays++;
             if (flag) break;
-            //System.out.println("curr level: " + numPlays);
-        }//
+        }
+        minPlays = numPlays;
         return flag;
     }
 
     public int getNumPlays() {
-        return numPlays;
+        return minPlays;
     }
 }
